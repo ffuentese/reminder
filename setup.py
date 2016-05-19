95dd336f83077a9d163ab602d1d75fd3bce83bc9
ffuentese <ffuentese@users.noreply.github.com>
2016-05-19 01:14:36 -0400

    Minor adjustments

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='Reminder',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='Beerware',
+    author='Francisco',
+    author_email='txi.francisco@gmail.com',
+    description='Password reminder (it \' s buggy)')
